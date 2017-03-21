46669536258352d5637480922a5f22b8e5d142e5
Ben Balter <ben.balter@github.com>
2017-03-21 18:56:13 -0400

    add basic tests

--- lib/zoom_launcher.rb
+++ lib/zoom_launcher.rb
@@ -3,68 +3,15 @@ require 'google/apis/calendar_v3'
 require 'action_view'
 require 'action_view/helpers'
 require 'colored'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
+require 'thor'
+require 'os'
 
-require_relative 'zoom_launcher/base_cli'
-require_relative 'zoom_launcher/event'
+require_relative 'google/apis/calendar_v3/event'
 
 module ZoomLauncher
-  class CLI < BaseCli
-    CALENDAR = 'primary'.freeze
-    ARGUMENTS = {
-      single_events: true,
-      order_by: 'startTime',
-      time_min: Time.now.iso8601,
-      q: 'zoom.us',
-      max_results: 5
-    }.freeze
-    default_task :launch
-
-    desc 'launch', 'Launch your next zoom meeting'
-    def launch
-      auth
-      if next_event.meeting_url
-        puts "Your next Zoom meeting is \"#{next_event.summary.bold}\"."
-        is_was = next_event.already_started? ? 'was' : 'is'
-        puts "It #{is_was} scheduled to start #{next_event.start_time_in_words}."
-        puts
-
-        if next_event.more_than_five_minutes_from_now?
-          puts "Here's the Zoom URL: #{next_event.meeting_url.bold}"
-        else
-          puts "Opening #{next_event.meeting_url.bold}..."
-          `open #{next_event.meeting_url}`
-
-        end
-        puts "Oh, and here's the URL in case you need it: #{next_event.html_link}"
-      else
-        puts "Can't find any upcomming Zoom meetings"
-      end
-    end
-
-    desc 'auth', 'authorize CLI with Google Calendar'
-    def auth
-      calendar.authorization = user_credentials_for(
-        Google::Apis::CalendarV3::AUTH_CALENDAR
-      )
-    end
-
-    desc 'logout', 'Deauthorize this computer from accessing your calendar'
-    def logout
-      FileUtils.rm_rf token_store_path
-    end
-
-    no_commands do
-      def calendar
-        @calendar ||= Google::Apis::CalendarV3::CalendarService.new
-      end
-
-      def events
-        @events ||= calendar.list_events(CALENDAR, ARGUMENTS)
-      end
-
-      def next_event
-        @next_event ||= events.items.first
-      end
-    end
-  end
+  autoload :CLI,        "zoom_launcher/cli"
+  autoload :GoogleAuth, "zoom_launcher/google_auth"
 end
---
+++ spec/zoom_launcher/cli_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe ZoomLauncher::CLI do
+  let(:api_base) { "https://www.googleapis.com/calendar/v3/calendars/primary/events?" }
+  let(:headers) { { 'Content-Type' => 'application/json' } }
+  let(:output) { capture(:stdout) { subject.launch } }
+  let(:args) do
+    {
+      "maxResults": 5,
+      "orderBy": "startTime",
+      "q": "zoom.us",
+      "singleEvents": true,
+      "timeMin": Time.now.iso8601
+    }
+  end
+
+  before do
+    url = api_base + args.map { |k,v| "#{k}=#{v}"}.join("&")
+    body = fixture_contents("events.json")
+    stub_request(:get, url).to_return(:status => 200, :body => body, :headers => headers)
+  end
+
+  it "creates the calendar" do
+    expect(subject.calendar).to be_a(Google::Apis::CalendarV3::CalendarService)
+  end
+
+  it "pulls events" do
+    expect(subject.events.kind).to eql("calendar#events")
+    items = subject.events.items
+    expect(items.count).to eql(1)
+    expect(items.first.location).to eql("https://foo.zoom.us/j/1245678")
+  end
+
+  it "pulls the next event" do
+    expect(subject.next_event.location).to eql("https://foo.zoom.us/j/1245678")
+  end
+
+  it "launches" do
+    expect(output).to include("Your next Zoom meeting is \"\e[1mTest Event\e[0m\"")
+    expect(output).to include("Here's the Zoom URL: \e[1mhttps://foo.zoom.us/j/1245678\e[0m")
+  end
+end
